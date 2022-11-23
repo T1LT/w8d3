eff84c8bff37302607d8f148f6ead9be4f4f3b5e
Nishant Racherla <nishant.racherla@gmail.com>
2022-11-23 15:38:23 -0800

    finished absurdBubbleSort

--- callback_exercises.js
+++ callback_exercises.js
@@ -56,7 +56,82 @@ function addNumbers(sum, numsLeft, completionCallback){
   }
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// reader.question('What do you think of JavaScript? ', answer => {
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+//   reader.question('What do you REALLY think of JavaScript? ', response => {
+//       console.log(`You said: ${response}. Thank you for your honesty.`);
+//       reader.close();
+//   });
+// });
+
+Function.prototype.myBind = function(context) {
+  return () => { this.apply(context); }
+}
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+  console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+// turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+// boundTurnOn(); // should say "Turning on a lamp"
+// myBoundTurnOn(); // should say "Turning on a lamp"
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} > ${el2}? `, (answer) => {
+    if (answer === "yes") {
+      callback(true);
+    } else {
+      callback(false);
+    }
+    // reader.close();
+  });
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], (isGreaterThan) => {
+      if (isGreaterThan) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        innerBubbleSortLoop(arr, i + 1, true, outerBubbleSortLoop);
+      } else {
+        innerBubbleSortLoop(arr, i + 1, false, outerBubbleSortLoop);
+      }
+    })
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
+}
+
+// innerBubbleSortLoop([2, 3, 1, 5, 4, 6], 1, false, () => { console.log("in outer bubble sort") })
+absurdBubbleSort([3, 2, 1], function (arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
+
 // reader.question('What do you think of JavaScript? ', answer => {
 //   console.log(`Thank you for your valuable feedback: ${answer}`);
 //   reader.question('What do you REALLY think of JavaScript? ', response => {
